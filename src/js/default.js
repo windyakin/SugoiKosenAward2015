807b00821a7b5ced813777abcb49532a2b181b9b
windyakin <windyakin@gmail.com>
2015-12-30 22:36:58 +0900

    主にJavaScript関連

--- src/js/default.js
+++ src/js/default.js
@@ -1,4 +1,4 @@
-(function($, window, undefined) {
+(function($, Chart, window, undefined) {
 
 	'use strict';
 
@@ -7,23 +7,73 @@
 		throw new Error('This JavaScript requires jQuery');
 	}
 
-	var obj, Obj = function() {
-		this.initalized();
-		return this;
-	};
-	Obj.prototype = {
-		initalized: function() {
-			this.hoge = 'hoge';
-			$(document)
-				.on('click', 'body', $.proxy(this.showThis, this));
-		},
-		showThis: function() {
-			console.log(obj);
-		}
-	};
 
 	$(document).ready(function() {
-		new Obj();
+		Chart.defaults.global.responsive = true;
+		var kosenData = {
+			ube: [5, 4.5, 5, 5, 5],
+			nagaoka: [4.5, 4.5, 5, 5, 5],
+			kanazawa: [5, 5, 5, 5, 5],
+			kisarazu: [4, 3.5, 5, 4.5, 3.5],
+			kochi: [5, 5, 5, 5, 5]
+		};
+		var base = {
+			labels: ['デザイン性', '更新頻度', '情報整理', 'アクセシビリティ', '中学生向け'],
+			datasets: [{
+				label: 'DataSet',
+				fillColor: 'rgba(151,187,205,0.2)',
+				strokeColor: 'rgba(151,187,205,1)',
+				pointColor: 'rgba(151,187,205,1)',
+				pointStrokeColor: '#fff',
+				pointHighlightFill: '#fff',
+				pointHighlightStroke: 'rgba(151,187,205,1)',
+			}]
+		};
+		$.each($('.evaluation-point'), function() {
+			var kosen = $(this).data('kosen');
+			var data = base;
+			data.datasets[0].data = kosenData[kosen];
+			var ctx = $(this).find('.radarchart').get(0).getContext('2d');
+			new Chart(ctx).Radar(data, {
+				scaleOverride: true,
+				scaleSteps: 5,
+				scaleStepWidth : 1,
+				scaleStartValue : 0
+			});
+			var html = $('#announceTemplate').render(
+			{'data': kosenData[kosen]},
+			{
+				getStar: function(point) {
+					var str = '';
+					var count = 0;
+					var star = [
+						'<i class="fa fa-lg fa-star"></i>',
+						'<i class="fa fa-lg fa-star-half-o"></i>',
+						'<i class="fa fa-lg fa-star-o"></i>'
+					];
+					for (var i = 0; i < Math.floor(point); i++) {
+						str += star[0];
+						count++;
+					}
+					if (point % 1 === 0.5) {
+						str += star[1];
+					}
+					for (var i = count; i < count; i++) {
+						str += star[2];
+					}
+					return str;
+				},
+				sumArray: function(array) {
+					var sum = 0;
+					for (var i = 0; i < array.length; i++) {
+						sum += array[i];
+					}
+					return sum;
+				}
+			});
+			$(this).find('.evaluation-point-table').html(html);
+		});
 	});
 
-})(jQuery, window, void 0);
+
+})(jQuery, Chart, window, void 0);
